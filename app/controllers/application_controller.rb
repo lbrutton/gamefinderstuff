cb59f60025030b560030e768a17b618d670e944d
Louis Brutton <louisbrutton@gmail.com>
2014-07-26 11:59:24 +0200

    add basic buttons with links for each game

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,4 +24,13 @@ class ApplicationController < ActionController::Base
 		end
 	end
 
+	def make_link(game)
+		if game.type == "IosGame"
+			link = "https://itunes.apple.com/app/id#{game.store_application_id}"
+		else
+			link = "https://play.google.com/store/apps/details?id=#{game.store_application_id}"
+		end
+	link
+	end
+
 end
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,6 +11,7 @@ class GamesController < ApplicationController
 	def show
 		@game = Games.find_by(id:params[:id])
 		@game_detail = GameDetails.find_by(game_id: @game.id, language_id: session[:language])
+		@link = make_link(@game)
 	end
 
 end
